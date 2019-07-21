6d4bf68dd7e89661277d19185c0e0757297718bb
wyang <wyang1117@msn.com>
2019-07-21 14:27:53 +0800

    完成crud的操作并渲染到页面上

---
+++ application/index/config.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wangyang
+ * Date: 2019/7/21
+ * Time: 11:14
+ */
+
+//配置文件
+return [
+    'view_replace_str' => [
+        '__WEBSITE__'  => '/static/webSite',
+        '__PUBLIC__'   => '/static/public',
+        '__COMMONCSS__'=> '/static/webSite/common/css',
+        '__COMMONJS__' => '/static/webSite/common/js',
+    ],
+];
--- application/index/model/UserModel.php
+++ application/index/model/UserModel.php
@@ -8,14 +8,31 @@
 
 namespace app\index\model;
 
+use app\index\controller\User;
 use think\Db;
 use think\Model;
 
 class UserModel extends Model
 {
+    public function addUser($name)
+    {
+        $isExist = Db::table('study_user')->where('name',$name)->find();
+        if (!$isExist){
+            $userName = Db::table('study_user')->insert(['name' => $name]);
+            if ($userName){
+                return ['code'=>200,'msg'=>'添加成功'];
+            }
+        }else{
+            return ['code'=>201,'msg'=>'添加失败'];
+        }
+
+    }
+
     public function getUser()
     {
-        $uid = Db::table('study_user')->where('id', 1)->find();
-        return $uid;
+        $users = Db::table('study_user')->select();
+        if (!empty($users)) {
+            return $users;
+        }
     }
 }
