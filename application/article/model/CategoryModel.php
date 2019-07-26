c7f3e71321bf7581e8e33238ca2060ce6564c90a
wyang <wyang1117@msn.com>
2019-07-27 00:07:58 +0800

    添加新增数据接口

--- application/article/model/CategoryModel.php
+++ application/article/model/CategoryModel.php
@@ -34,4 +34,43 @@ class CategoryModel extends Model
         }
     }
 
+    public function addData()
+    {
+        $data = [
+            ['pid' => 0, 'title' => '散文随笔', 'create_time' => Null, 'update_time' => Null],
+            ['pid' => 1, 'title' => '优美散文', 'create_time' => Null, 'update_time' => Null],
+            ['pid' => 1, 'title' => '抒情散文', 'create_time' => Null, 'update_time' => Null],
+            ['pid' => 1, 'title' => '爱情散文', 'create_time' => Null, 'update_time' => Null],
+            ['pid' => 1, 'title' => '经典散文', 'create_time' => Null, 'update_time' => Null],
+            ['pid' => 1, 'title' => '伤感散文', 'create_time' => Null, 'update_time' => Null],
+            ['pid' => 0, 'title' => '伤感文章', 'create_time' => Null, 'update_time' => Null],
+            ['pid' => 7, 'title' => '伤感日志', 'create_time' => Null, 'update_time' => Null],
+            ['pid' => 7, 'title' => '伤感日记', 'create_time' => Null, 'update_time' => Null],
+            [ 'pid' => 7, 'title' => '感人故事', 'create_time' => Null, 'update_time' => Null],
+            [ 'pid' => 7, 'title' => '伤感故事', 'create_time' => Null, 'update_time' => Null],
+            [ 'pid' => 7, 'title' => '私密关系', 'create_time' => Null, 'update_time' => Null],
+            [ 'pid' => 0, 'title' => '诗词歌赋', 'create_time' => Null, 'update_time' => Null],
+            [ 'pid' => 13, 'title' => '散文诗', 'create_time' => Null, 'update_time' => Null],
+            [ 'pid' => 13, 'title' => '现代诗歌', 'create_time' => Null, 'update_time' => Null],
+            [ 'pid' => 13, 'title' => '爱情诗歌', 'create_time' => Null, 'update_time' => Null],
+            [ 'pid' => 13, 'title' => '爱国诗歌', 'create_time' => Null, 'update_time' => Null],
+            [ 'pid' => 13, 'title' => '格律诗', 'create_time' => Null, 'update_time' => Null],
+            [ 'pid' => 13, 'title' => '古词风韵', 'create_time' => Null, 'update_time' => Null],
+            [ 'pid' => 0, 'title' => '情感日志', 'create_time' => Null, 'update_time' => Null],
+            [ 'pid' => 20, 'title' => '情感美文', 'create_time' => Null, 'update_time' => Null],
+            [ 'pid' => 20, 'title' => '情感日记', 'create_time' => Null, 'update_time' => Null],
+            [ 'pid' => 20, 'title' => '情感故事', 'create_time' => Null, 'update_time' => Null],
+            [ 'pid' => 20, 'title' => '美文欣赏', 'create_time' => Null, 'update_time' => Null],
+            [ 'pid' => 20, 'title' => '爱情文章', 'create_time' => Null, 'update_time' => Null],
+            [ 'pid' => 20, 'title' => '亲情文章', 'create_time' => Null, 'update_time' => Null],
+            [ 'pid' => 20, 'title' => '友情文章', 'create_time' => Null, 'update_time' => Null],
+            [ 'pid' => 0, 'title' => '心情日记', 'create_time' => Null, 'update_time' => Null],
+            [ 'pid' => 28, 'title' => '心情随笔', 'create_time' => Null, 'update_time' => Null],
+            [ 'pid' => 28, 'title' => '心情日志', 'create_time' => Null, 'update_time' => Null],
+        ];
+
+        $add = $this->saveAll($data);
+        return ['code' => 200, 'msg' => '查询成功', 'data' => $add];
+    }
+
 }
